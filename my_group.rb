11316641c3dd44f2af4b57f44a161a916aade273
George_Lewis <georgejlewis@hotmail.com>
2018-04-03 17:06:51 +0200

    Adding my group to git hub

---
+++ my_group.rb
@@ -0,0 +1,11 @@
+# my_group.rb
+
+my_group = ["person_1", "person_2", "person_3"]
+person_1 = {name: "Dave", gender: "male", age: 22}
+person_2 = {name: "Chris", gender: "male", age: 33}
+person_3 = {name: "Karen", gender: "female", age: 45}
+names.each do |name|
+  age.each do |age|
+    gender.each do |gender|
+puts "#{name} is a #{age} year old #{gender}"
+end
